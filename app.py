4534e31bef90c3e61c2f2471f5b6cf447bddb3d9
jakob25 <jacobawilcox00@gmail.com>
2026-03-25 21:40:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -165,7 +165,7 @@ h1, h2, h3 {
     font-weight: 800;
     font-style: italic;
     background: linear-gradient(45deg, #44ddff, #aa00ff, #00aaff, #0066ff, #00ccff, #44ddff,  #aa00ff, #00aaff, #0066ff, #00ccff);
-    background-size: 200% auto;
+    background-size: 400% auto;
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
     background-clip: text;
